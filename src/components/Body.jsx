73b236513b99b4b37b85f75cbf3c0e14004600be
damyantjain <damyant9999@gmail.com>
2024-07-02 11:05:58 -0400

    Added search functionality and shimmer update

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,8 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [resList, setResList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredResList, setFilteredResList] = useState([]);
 
   useEffect(() => {
     getRestaurants();
@@ -17,24 +19,42 @@ const Body = () => {
     var resCard = res?.data?.cards?.filter((c) => c.card.card.id == "top_brands_for_you");
     var restaurants = resCard[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
     setResList(restaurants);
+    setFilteredResList(restaurants);
   };
+
+  const serachRestaurant = () => {
+    if(searchText === ""){
+      setFilteredResList(resList);
+      return;
+    }
+    setFilteredResList(resList.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())));
+  }
   if(resList.length === 0){
     return <Shimmer />
   }
   return (
     <div className="body">
       <div className="filter">
-        <button
-          className="btn btn-primary mt-2 ms-2"
-          onClick={() => {
-            setResList(resList.filter((res) => res.info.avgRating > 4.3));
-          }}
-        >
+        <div className="search row form-row align-items-center mt-2">
+          <div className="col-sm-3">
+            <input type="text" className="search-bar form-control" 
+            placeholder="Search Restaurants" 
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)} /> 
+          </div>
+          <button className="btn btn-success col-auto" onClick={serachRestaurant}>Search</button>
+          <button
+            className="btn btn-primary col-auto ms-2"
+            onClick={() => {
+              setFilteredResList(resList.filter((res) => res.info.avgRating > 4.3));
+            }}
+          >
           Top Rated Restaurants
         </button>
+        </div>
       </div>
       <div className="row">
-        {resList?.map((res) => (
+        {filteredResList?.map((res) => (
           <div key={res.info.id} className="col-lg-3 col-md-4 col-sm-6 col-12">
             <RestaurantCard res={res.info} />
           </div>
--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,21 +1,22 @@
+
+const ShimmerCard = () => {
+    return (
+        <div className="col-lg-3 col-md-4 col-sm-6 col-12">
+            <div className="shimmer-card"></div>
+            <div className="shimmer-details">
+                <div className="shimmer-line shimmer-md"></div>
+                <div className="shimmer-line shimmer-sm"></div>
+            </div>
+        </div>
+    )
+}
+
 const Shimmer = () => {
     return (
         <div className="row">
-            <div className="col-lg-3 col-md-4 col-sm-6 col-12">
-                <div className="shimmer-card"></div>
-            </div>
-            <div className="col-lg-3 col-md-4 col-sm-6 col-12">
-                <div className="shimmer-card"></div>
-            </div>
-            <div className="col-lg-3 col-md-4 col-sm-6 col-12">
-                <div className="shimmer-card"></div>
-            </div>
-            <div className="col-lg-3 col-md-4 col-sm-6 col-12">
-                <div className="shimmer-card"></div>
-            </div>
-            <div className="col-lg-3 col-md-4 col-sm-6 col-12">
-                <div className="shimmer-card"></div>
-            </div>
+            {[...Array(12)].map((_, i) => (
+                <ShimmerCard key={i} />
+            ))}
         </div>
     );
 }
