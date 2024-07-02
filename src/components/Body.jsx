2d3537216e818b5000e0ef46c3c3ad2cd805da54
damyantjain <damyant9999@gmail.com>
2024-07-02 13:36:24 -0400

    Added cors proxy

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,14 +6,26 @@ const Body = () => {
   const [resList, setResList] = useState([]);
   const [searchText, setSearchText] = useState("");
   const [filteredResList, setFilteredResList] = useState([]);
-
   useEffect(() => {
     getRestaurants();
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        updateRestaurants();
+      }
+    };
+  
+    window.addEventListener('scroll', handleScroll);
+  
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [resList]);
+
+
   const getRestaurants = async () => {
     var data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5961173&lng=77.0613177&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5961173&lng=77.0613177&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     var res = await data.json();
     var resCard = res?.data?.cards?.filter((c) => c.card.card.id == "top_brands_for_you");
@@ -21,6 +33,26 @@ const Body = () => {
     setResList(restaurants);
     setFilteredResList(restaurants);
   };
+
+  // const updateRestaurants = async () => {
+  //   var data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/update",{
+  //     method: "POST",
+  //     headers: {
+  //       "content-type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       lat: "28.572341",
+  //       lng: "77.0776524",
+  //       nextOffset: "CJhlELQ4KIDgjueQpd/FcjCnEzgE",
+  //       page_type: "DESKTOP_WEB_LISTING",
+  //     }),
+  //   })
+  //   var res = await data.json();
+  //   var resCard = res?.data?.cards?.filter((c) => c.card.card.id == "top_brands_for_you");
+  //   var restaurants = resCard[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+  //   setResList([...resList, ...restaurants]);
+  //   setFilteredResList([...resList, ...restaurants]);
+  // }
 
   const serachRestaurant = () => {
     if(searchText === ""){
