aa34848418c921883c525dec0d145fc40f1f6522
damyantjain <damyant9999@gmail.com>
2024-09-07 12:48:54 -0400

    Removed contact from App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Body from "./components/Body";
 import About from "./components/About";
 import RestaurantMenu from "./components/RestaurantMenu";
 import Error from "./components/Error";
-import Contact from "./components/Contact";
 import ShimmerRestaurant from "./components/ShimmerRestaurant";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import UserContext from "./utils/UserContext";
